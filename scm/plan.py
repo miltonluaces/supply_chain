a6ff8bcb6f47480f4dcef820ec22b8bfc314ac9b
miltonluaces <miltonluacs@gmail.com>
2021-03-20 10:57:16 +0000

    Planning optimization

---
+++ scm/plan.py
@@ -0,0 +1,96 @@
+import sys
+sys.path.append('D:/source/repos')
+from utilities.std_imports import *
+pd.set_option('display.max_columns', None)
+
+class Plan:
+    
+    def __init__(self, horizon, dmd, on_hand):
+        self.horizon = horizon
+        self.plan = pd.DataFrame(columns=list(range(horizon+1)))
+        self.plan.loc[0] = dmd
+        self.plan.loc[1] = np.zeros(horizon+1)
+        stk = np.zeros(horizon+1)
+        stk[0] = on_hand
+        self.plan.loc[2] = stk
+        self.plan.index = ['Dmd', 'Pl.Or', 'Stock']
+        
+        self.hol_uc = 0
+        self.tr_fix_uc = 0
+        self.tr_var_uc = 0
+        
+    def set_costs(self, hol_uc, tr_fix_uc, tr_var_uc):
+        self.hol_uc = hol_uc
+        self.tr_fix_uc = tr_fix_uc
+        self.tr_var_uc = tr_var_uc
+        
+    def show(self):
+        display(self.plan)       
+        
+    def upd_stk(self):
+        for i in range(1, self.horizon+1):
+            self.plan.loc['Stock'][i] = self.plan.loc['Stock'][i-1] - self.plan.loc['Dmd'][i] + self.plan.loc['Pl.Or'][i]
+    
+    def get_tot_dmd(self):
+        return self.plan.loc['Dmd'].sum()
+    
+    def get_avg_stk(self):
+        return round(self.plan.loc['Stock'].mean(),2)
+    
+    def get_npo(self):
+        return len(self.plan.loc['Pl.Or'][self.plan.loc['Pl.Or']>0])
+ 
+    def get_hol_cost(self, end=-1):
+        if end == 0: return 0
+        if end ==-1: end = self.horizon
+        stk_pos = self.plan.loc['Stock'][:end+1][self.plan.loc['Stock'][:end+1]>0].sum()
+        hol_cost = round(stk_pos * self.hol_uc,2)
+        return round(hol_cost/end,2)
+    
+    def get_tr_cost(self, end=-1):
+        if end == 0: return 0
+        if end==-1: end = self.horizon
+        po = self.plan.loc['Pl.Or'][:end+1][self.plan.loc['Pl.Or'][:end+1]>0]
+        n_po = len(po)
+        tot_po = po.sum()
+        tr_fix = n_po * self.tr_fix_uc
+        tr_var = tot_po * self.tr_var_uc
+        tr = tr_fix + tr_var
+        return round(tr/end,2)
+    
+    def get_tot_cost(self, end=-1):
+        return round(self.get_hol_cost(end) + self.get_tr_cost(end),2) 
+    
+    def set_po(self, po):
+        self.plan.loc['Pl.Or'] = po
+        self.upd_stk()
+        
+    def show_metrics(self):
+        tot_dmd = self.get_tot_dmd()
+        avg_stk = self.get_avg_stk()
+        n_po = self.get_npo()
+        hol_cost = self.get_hol_cost()
+        tr_cost = self.get_tr_cost()
+        tot_cost = self.get_tot_cost()
+
+        metric_names = ['Total demand', 'Average stock', 'N Pl.orders', 'Holding cost', 'Transport cost', 'Total cost']
+        metric_values = [tot_dmd, avg_stk, n_po, hol_cost, tr_cost, tot_cost]
+        met = pd.DataFrame({'Metric': metric_names, 'Value': metric_values})
+        display(met)
+        
+    def get_hol_cost_ac(self):
+        return  [self.get_hol_cost(end) for end in range(self.horizon+1)]
+    
+    def get_tr_cost_ac(self):
+        return  [self.get_tr_cost(end) for end in range(self.horizon+1)]
+    
+    def get_tot_cost_ac(self):
+        return  [self.get_tot_cost(end) for end in range(self.horizon+1)]
+    
+    def show_ac_costs(self):
+        plan_costs = self.plan
+        plan_costs.loc[3] = self.get_hol_cost_ac()
+        plan_costs.loc[4] = self.get_tr_cost_ac()
+        plan_costs.loc[5] = self.get_tot_cost_ac()
+        plan_costs.index = ['Dmd', 'Pl.Or', 'Stock', 'Ho_cost', 'Tr_cost', 'Tot_cost']
+        display(plan_costs)
---
+++ scm/plan_algorithms.py
@@ -0,0 +1,85 @@
+import sys
+sys.path.append('D:/source/repos')
+from utilities.std_imports import *
+pd.set_option('display.max_columns', None)
+from supply_chain.scm.plan import Plan
+
+
+def brute_force_planning(po, max_npo, min_repl, max_repl, horizon):
+    def bfp(po, npo, p, pos):
+        pO = np.copy(po)
+        if p==horizon or npo==max_npo: 
+            #print('Complete ', npo)
+            pos.append(pO)
+            return
+        #print(npo)
+        for r in range(min_repl, max_repl+1):
+            if(p+r > horizon):
+                continue
+            pO = np.copy(po)
+            pO[p+r] = 1
+            bfp(pO, npo+1, p+r, pos)
+
+    pos = []            
+    bfp(po=po, npo=1, p=3, pos=pos)
+    return pos
+
+# given plan and pos_pos generate pos
+def gen_po(plan, pos_idx):
+    stock_ini = plan.plan.loc['Stock'][pos_idx[0]-1]
+    pos = []
+    for i in range(0, len(pos_idx)-1):
+         pos.append(plan.plan.loc['Dmd'][pos_idx[i]:pos_idx[i+1]].sum())
+
+    po = np.zeros(plan.horizon+1)
+    for i in range(0, len(pos_idx)-1):
+        po[pos_idx[i]] = pos[i]
+    return po
+
+
+def get_first_neg(plan):
+    for p in range(0, plan.horizon+1):
+        if plan.plan.loc['Stock'][p]<=0: 
+            return p
+
+        
+def pbin2pidx(pb):
+    pb[len(pb)-1] = 1
+    return np.where(pb==1)[0]
+
+
+def plan_optimization(plan, pbin):
+    best_po = None
+    best_cost = np.inf
+    for pb in pbin:
+        pidx = pbin2pidx(pb)
+        po = gen_po(plan, pidx)
+        plan.set_po(po)
+        cost = plan.get_tot_cost()
+        if cost < best_cost:
+            best_cost = cost
+            best_po = po
+    return best_cost, best_po
+        
+    
+    
+# npo = random between 1 and 6
+# random no rep npo between [firstPo, 20], with firstPo in first negative
+# calculate order quantitites (aritmetics)
+
+# another possibility: sequence of periods: 2,3,7 (until 20)
+
+# another posssibility: backtracking 0/1 prune on increasing 
+
+
+
+# Minimize total cost while avoiding stockouts
+
+# display hol, tr fix , tr var and total cost per period of a Planning solution (pl orders)
+# each time goes to zero if that subplan is optimal it will continue being for overall plan (?) No.
+# top down: one order covering all, then split if it improves cost until it doesnt
+
+# Reorder point is pure arithmetics so quantities is the independent variable. Should be equal sum of dmds so, number of periods. Sequence of number of periods 
+# until horizon
+
+# Linear programming?
